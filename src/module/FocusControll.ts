942721f67dffe3ed099cd3a56b5f346bec7f93e9
m-kur <m@botsignal.com>
2019-12-17 14:14:38 +0900

    削除もれ

--- src/module/FocusControll.ts
+++
@@ -1,9 +0,0 @@
-import { useState, RefObject } from 'react';
-import { createContainer } from 'unstated-next';
-
-const useFocusControll = (initialState?: boolean) => {
-    const initialValue = Boolean(initialState);
-    return useState(initialValue);
-};
-
-export default createContainer(useFocusControll);
